c6177b4e05afcf9b6dcb9ae12ef49fb72f671ee8
Misbahkanwal06 <misbahkan26@gmail.com>
2024-07-25 08:14:49 -0700

    Signup Form

--- src/Component/Auth/Signup.jsx
+++ src/Component/Auth/Signup.jsx
@@ -1,30 +1,31 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Card, Button, Form } from 'react-bootstrap';
+import './Style.css' ;
 
 function SignupForm() {
     return (
         <Container className='d-flex align-items-center justify-content-center' style={{ minHeight: '100vh' }}>
-            <Card style={{ maxWidth: '600px', width: '100%' }}>
+            <Card style={{ maxWidth: '400px', width: '100%' }}>
                 <Card.Body>
-                    <h2 className="text-center mb-5">Create an account</h2>
+                    <h2 className="text-center mb-5">Create an Account</h2>
                     <Form>
                         <Form.Group className="mb-4" controlId="formName">
-                            <Form.Label>Your Name</Form.Label>
-                            <Form.Control type="text" placeholder="Enter your name" />
+                            <Form.Label className="d-flex justify-content-start">User Name</Form.Label>
+                            <Form.Control type="text" />
                         </Form.Group>
 
                         <Form.Group className="mb-4" controlId="formEmail">
-                            <Form.Label>Your Email</Form.Label>
-                            <Form.Control type="email" placeholder="Enter your email" />
+                            <Form.Label className="d-flex justify-content-start">User Email</Form.Label>
+                            <Form.Control type="email" />
                         </Form.Group>
 
                         <Form.Group className="mb-4" controlId="formPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Enter your password" />
+                            <Form.Label className="d-flex justify-content-start">Password</Form.Label>
+                            <Form.Control type="password" />
                         </Form.Group>
-
-                        <Button className="w-100" variant="primary" size="lg">Register</Button>
+                        <Button className="w-100 custom-button" size="lg">Register</Button>
                     </Form>
                 </Card.Body>
             </Card>
