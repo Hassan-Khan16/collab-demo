1804a7762a8739f199edaf4bfa7294ce7c4cc172
Hassan Khan <hassankpisces@gmail.com>
2024-07-26 05:02:47 -0700

    Footer completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Signup from "../Component/Auth/Signup"
-
+import Footer from '../layouts/Footer.jsx'
 
 const Home = () => {
     return (
         <>
             <Signup />
+            <Footer />
         </>
     );
 };
