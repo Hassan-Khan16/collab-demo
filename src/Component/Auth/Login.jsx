c0bd8c4270604c351a462f3ad20ed5c92cf8bd07
Hassan Khan <hassankpisces@gmail.com>
2024-07-26 03:07:13 -0700

    changes to test

--- src/Component/Auth/Login.jsx
+++ src/Component/Auth/Login.jsx
@@ -1,46 +1,84 @@
-
-
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
-import { Container, Card, Button, Form } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown ,Container, Card, Button, Form } from 'react-bootstrap';
 import './Style.css';
 
 function Login() {
     return (
+        <>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container fluid>
+                    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link href="#action1">Home</Nav.Link>
+                            <Nav.Link href="#action2">Link</Nav.Link>
+                            <NavDropdown title="Link" id="navbarScrollingDropdown">
+                                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action4">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action5">
+                                    Something else here
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="#" disabled>
+                                Link
+                            </Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
 
-        <Container className='d-flex align-items-center justify-content-center container-center' >
-            <Card className='card-max-width'>
-                <Card.Body>
-                    <h2 className="text-center mb-5">Login to your Account</h2>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicUsername">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" placeholder="Enter username" />
-                        </Form.Group>
+            <Container className='d-flex align-items-center justify-content-center container-center' >
+                <Card className='card-max-width'>
+                    <Card.Body>
+                        <h2 className="text-center mb-5">Login to your Account</h2>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formBasicUsername">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control type="text" placeholder="Enter username" />
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                        </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
 
-                        <div className="d-grid">
-                            <Button className='custom-button ' size="lg">
-                                Login
-                            </Button>
-                        </div>
-                        <div className='text-center m-3'>
-                            <p>Don't have an account? <Link to="/" style={{textDecoration:"none"}}>Sign up</Link></p>
-                        </div>
-                    </Form>
-                </Card.Body>
-            </Card>
-        </Container>
+                            <div className="d-grid">
+                                <Button className='custom-button ' size="lg">
+                                    Login
+                                </Button>
+                            </div>
+                            <div className='text-center m-3'>
+                                <p>Don't have an account? <Link to="/" style={{ textDecoration: "none" }}>Sign up</Link></p>
+                            </div>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </Container>
+        </>
     );
 }
 
