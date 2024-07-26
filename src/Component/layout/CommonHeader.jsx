672db29f47ffa57d8bafbc3abd2cc4724ae0b238
Misbahkanwal06 <misbahkan26@gmail.com>
2024-07-26 05:32:37 -0700

    header completed

--- src/Component/layout/CommonHeader.jsx
+++ src/Component/layout/CommonHeader.jsx
@@ -1,24 +1,32 @@
+
+
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import './Styles.css';
 
-function CommonHeader() {
+const CustomHeader = () => {
     return (
-        <Navbar bg="primary" variant="dark" expand="lg">
-            <Navbar.Brand href="/">My Application</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
+        <Navbar bg="dark" variant="dark" expand="lg" className="custom-navbar">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-toggler" />
+            <Navbar.Collapse id="basic-navbar-nav" className="custom-collapse">
+                <Nav className="mx-auto">
                     <LinkContainer to="/">
-                        <Nav.Link>Home</Nav.Link>
+                        <Nav.Link className="nav-item">Home</Nav.Link>
                     </LinkContainer>
                     <LinkContainer to="/about">
-                        <Nav.Link>About</Nav.Link>
+                        <Nav.Link className="nav-item">About Us</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/signup">
+                        <Nav.Link className="nav-item">Sign Up</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/login">
+                        <Nav.Link className="nav-item">Login</Nav.Link>
                     </LinkContainer>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
     );
-}
+};
 
-export default CommonHeader;
+export default CustomHeader;
