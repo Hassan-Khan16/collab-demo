62d178b63f0a3149022d762cb93cff20853a6270
Hassan Khan <hassankpisces@gmail.com>
2024-07-26 06:42:26 -0700

    footer links removed

--- src/Component/layout/CommonFooter.jsx
+++ src/Component/layout/CommonFooter.jsx
@@ -8,7 +8,7 @@ const CommonFooter = () => {
             <Navbar expand="lg" className="custom-theme-color custom-footer">
                 <Container fluid className='d-flex flex-column align-items-center justify-content-around m-3'>
                     <div className='d-flex flex-column flex-lg-row justify-content-around w-100 mb-2 text-center text-lg-left'>
-                        <a href="#" className='mb-2 mb-lg-0'>Terms & Conditions</a>
+                        <a href="#" className='mb-2 mb-lg-0 '>Terms & Conditions</a>
                         <p className='mb-2 mb-lg-0'>Copyright © 2024 Your College Concierge. All rights reserved.</p>
                         <a href="#" className='mb-2 mb-lg-0'>Privacy Policy</a>
                     </div>
