5c555e82bb4d655eccc91cc2f6a732dd38c8ed6c
Misbahkanwal06 <misbahkan26@gmail.com>
2024-07-26 02:26:44 -0700

    layout files are made now start making footer and header components

---
+++ src/Component/layout/CommonFooter.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const CommonFooter = () => {
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default CommonFooter;
--- src/routes/IndexRoutes.jsx
+++ src/routes/IndexRoutes.jsx
@@ -1,15 +1,23 @@
 
+
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../Component/Auth/Login';
-
+import Header from '../layouts/Header';
+import Footer from '../layouts/Footer';
 const IndexRoutes = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-        </Routes>
+        <div className="d-flex flex-column min-vh-100">
+            <Header />
+            <main className="flex-grow-1">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </main>
+            <Footer />
+        </div>
     );
 };
 export default IndexRoutes;
